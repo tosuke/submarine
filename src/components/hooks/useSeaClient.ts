af538966b039326805791f706a1fd382c49e3753
Tosuke <tasukeprg@gmail.com>
2019-06-29 22:32:49 +0900

    refactor(hooks): remove unused exports

--- src/components/hooks/useSeaClient.ts
+++ src/components/hooks/useSeaClient.ts
@@ -1,9 +1,6 @@
-import { createContext } from 'react'
 import { SeaClient } from '../../infra/seaClient'
 import { useAuthBloc } from './useAuthBloc'
 import { useValueObservable } from './useObservable'
-
-export const SeaClientContext = createContext<SeaClient | undefined>(undefined)
 
 export function useSeaClient(): SeaClient {
   const authBloc = useAuthBloc()
