c991088f0d13185a2a3320543d0f38923432a6fc
Tosuke <tasukeprg@gmail.com>
2019-07-07 13:41:00 +0900

    fix: save non react statics

--- src/components/hocs/withNavigationOption.ts
+++ src/components/hocs/withNavigationOption.ts
@@ -1,14 +1,15 @@
 import React from 'react'
 import { NavigationScreenOptions, NavigationScreenProp, NavigationRoute, NavigationParams } from 'react-navigation'
+import hoistNonReactStatics from 'hoist-non-react-statics'
 
-type OptionType =
+export type NavigationOptionType =
   | NavigationScreenOptions
   | ((args: {
       navigation: NavigationScreenProp<NavigationRoute<NavigationParams>, NavigationParams>
       navigationOptions: NavigationScreenOptions
     }) => NavigationScreenOptions)
 
-export function withNavigationOptions(options: OptionType): <P>(c: React.ComponentType<P>) => React.ComponentType<P> {
+export function withNavigationOptions(options: NavigationOptionType): <P>(c: React.ComponentType<P>) => React.ComponentType<P> & { readonly navigationOptions: NavigationOptionType } {
   return <P>(C: React.ComponentType<P>) => {
     class WithNavigationOptions extends React.Component<P> {
       static navigationOptions = options
@@ -21,6 +22,6 @@ export function withNavigationOptions(options: OptionType): <P>(c: React.Compone
         return React.createElement(C, this.props)
       }
     }
-    return WithNavigationOptions
+    return hoistNonReactStatics(WithNavigationOptions, C, { navigationOptions: true })
   }
 }
