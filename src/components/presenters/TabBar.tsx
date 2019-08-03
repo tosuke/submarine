91fceda1ef73b37935c11ecc5b9d6ee79f209bfd
Tosuke <tasukeprg@gmail.com>
2019-08-03 18:24:19 +0900

    refactor(presenters/TabBar): stop to use inline styles

--- src/components/presenters/TabBar.tsx
+++ src/components/presenters/TabBar.tsx
@@ -1,20 +1,27 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { BottomTabBar, BottomTabBarProps } from 'react-navigation'
 import { useTheme } from '../hooks/useTheme'
+import { TextStyle, ViewStyle } from 'react-native'
+
+const labelStyle: TextStyle = {
+  fontSize: 12,
+}
 
 export const TabBar: React.FC<BottomTabBarProps> = props => {
   const theme = useTheme()
+  const style = useMemo<ViewStyle>(
+    () => ({
+      backgroundColor: theme.colors.surface,
+    }),
+    [theme],
+  )
   return (
     <BottomTabBar
       {...props}
       activeTintColor={theme.colors.primary}
       inactiveTintColor={theme.colors.text}
-      labelStyle={{
-        fontSize: 12,
-      }}
-      style={{
-        backgroundColor: theme.colors.surface,
-      }}
+      labelStyle={labelStyle}
+      style={style}
     />
   )
 }
