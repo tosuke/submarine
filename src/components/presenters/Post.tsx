e5bc7d6c73e24d35eb4a46c77e41d34963f170bd
Tosuke <tasukeprg@gmail.com>
2019-08-05 01:44:40 +0900

    refactor(presenters/Post): use object literal instead of StyleSheet

--- src/components/presenters/Post.tsx
+++ src/components/presenters/Post.tsx
@@ -1,84 +1,103 @@
 import React from 'react'
-import { View, Image, StyleSheet } from 'react-native'
+import { View, Image, ImageStyle, ViewStyle, TextStyle } from 'react-native'
 import { Text, Caption, Paragraph } from 'react-native-paper'
 import { Post as PostType, File as FileType } from '../../models'
 
-const styles = StyleSheet.create({
-  avatar: {
-    marginRight: 4,
-    borderRadius: 4,
-    width: 32,
-    height: 32,
-  },
-  post: {
-    paddingVertical: 5,
-    flex: 1,
-    flexDirection: 'row',
-  },
-  postBody: {
-    flex: 1,
-    flexDirection: 'column',
-  },
-  postStatusView: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  postStatusNameView: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  postStatusNameText: {
-    marginRight: 5,
-    fontSize: 14,
-  },
-  postStatusScreenNameText: {
-    fontWeight: '200',
-  },
-  postText: {
-    flex: 1,
-  },
-})
+const avatarStyle: ImageStyle = {
+  marginRight: 4,
+  borderRadius: 4,
+  width: 32,
+  height: 32,
+}
 
 const AvatarImage: React.FC<{ avatarFile: FileType }> = ({ avatarFile }) => {
   const variant = avatarFile.variants.find(v => v.type === 'thumbnail' && v.extension !== 'webp')!
-  return <Image style={styles.avatar} source={{ uri: variant.url, width: 32, height: 32 }} />
+  return <Image style={avatarStyle} source={{ uri: variant.url, width: 32, height: 32 }} />
+}
+
+const avatarTextViewStyle: ViewStyle = {
+  ...avatarStyle,
+  backgroundColor: '#000',
+  justifyContent: 'center',
+  alignItems: 'center',
+}
+
+const avatarTextTextStyle: TextStyle = {
+  color: '#fff',
+  fontSize: 16,
+  lineHeight: 32,
 }
 
 const AvatarText: React.FC<{ text: string }> = ({ text }) => (
-  <View style={[styles.avatar, { backgroundColor: '#000', justifyContent: 'center', alignItems: 'center' }]}>
-    <Text style={{ color: '#fff', fontSize: 16, lineHeight: 32 }}>{text}</Text>
+  <View style={avatarTextViewStyle}>
+    <Text style={avatarTextTextStyle}>{text}</Text>
   </View>
 )
 
-const File: React.FC<{ file: FileType }> = ({ file }) => {
-  const variant = file.variants.find(v => v.type === 'thumbnail' && v.extension !== 'webp')!
-  return <Image style={{ marginRight: 4, borderRadius: 4 }} source={{ uri: variant.url, width: 128, height: 128 }}/>
+const fileImageStyle: ImageStyle = {
+  marginRight: 4,
+  borderRadius: 4,
 }
 
-export const Post: React.ComponentType<{ post: PostType, relativeTime: string}> = ({ post, relativeTime}) => (
-  <View style={styles.post}>
+const File: React.FC<{ file: FileType }> = ({ file }) => {
+  const variant = file.variants.find(v => v.type === 'thumbnail' && v.extension !== 'webp')!
+  return <Image style={fileImageStyle} source={{ uri: variant.url, width: 128, height: 128 }} />
+}
+
+const postViewStyle: ViewStyle = {
+  paddingVertical: 5,
+  flex: 1,
+  flexDirection: 'row',
+}
+
+const postBodyViewStyle: ViewStyle = {
+  flex: 1,
+}
+
+const postStatusViewStyle: ViewStyle = {
+  flexDirection: 'row',
+}
+
+const postStatusTextStyle: TextStyle = {
+  marginRight: 5,
+  fontSize: 14,
+}
+
+const postStatusScreenNameTextStyle: TextStyle = {
+  ...postStatusTextStyle,
+  fontWeight: 'bold',
+}
+
+const postContentTextStyle: TextStyle = {
+  marginVertical: 0,
+}
+
+const postFileViewStyle: ViewStyle = {
+  flex: 1,
+  flexDirection: 'row',
+}
+
+export const Post: React.ComponentType<{ post: PostType; relativeTime: string }> = ({ post, relativeTime }) => (
+  <View style={postViewStyle}>
     {post.user.avatarFile ? <AvatarImage avatarFile={post.user.avatarFile} /> : <AvatarText text={post.user.name[0]} />}
-    <View style={styles.postBody}>
-      <View style={styles.postStatusView}>
-        <View style={styles.postStatusNameView}>
-          <Text style={[styles.postStatusNameText, { fontWeight: 'bold' }]}>{post.user.name}</Text>
-          <Text style={styles.postStatusNameText}>@{post.user.screenName}</Text>
-        </View>
-        <Text>{relativeTime}</Text>
+    <View style={postBodyViewStyle}>
+      <View style={postStatusViewStyle}>
+        <Text style={postStatusScreenNameTextStyle}>{post.user.name}</Text>
+        <Text style={postStatusTextStyle}>@{post.user.screenName}</Text>
       </View>
-      <Paragraph style={{ marginVertical: 0 }} selectable={true}>
+      <Paragraph style={postContentTextStyle} selectable={true}>
         {post.text}
       </Paragraph>
-      <View style={{ flex: 1, flexDirection: 'row' }}>
+      <View style={postFileViewStyle}>
         {post.files.map(file => (
           <File key={file.id} file={file} />
         ))}
       </View>
-      <Caption style={{ marginVertical: 0 }}>
+      <Caption style={postContentTextStyle}>
         via {post.application.name}
         {post.application.isAutomated ? '[BOT]' : null}
       </Caption>
     </View>
+    <Text>{relativeTime}</Text>
   </View>
 )
