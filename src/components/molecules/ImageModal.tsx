482e3b81544165872aa62f9663e9b80e573ce29b
Tosuke <tasukeprg@gmail.com>
2019-08-27 00:34:22 +0900

    feat: implement pinch gestures

--- src/components/molecules/ImageModal.tsx
+++ src/components/molecules/ImageModal.tsx
@@ -1,32 +1,75 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef, useMemo } from 'react'
 import { Image, Dimensions, ImageStyle } from 'react-native'
+import { PinchGestureHandler, State } from 'react-native-gesture-handler'
+import Animated from 'react-native-reanimated'
+const { Value, event, set, cond, eq } = Animated
+
+function useImageSizeStyle(thumbnailUri?: string): ImageStyle {
+  // width / height
+  const [aspect, setAspect] = useState(1)
+
+  useEffect(() => {
+    if (thumbnailUri == null) return
+    Image.getSize(thumbnailUri, (w, h) => setAspect(w / h), () => {})
+  }, [thumbnailUri])
+
+  const { width: screenWidth, height: screenHeight } = Dimensions.get('screen')
+  const screenAspect = screenWidth / screenHeight
+  return aspect > screenAspect
+    ? {
+        width: screenWidth,
+        height: screenWidth / aspect,
+      }
+    : {
+        width: screenHeight * aspect,
+        height: screenHeight,
+      }
+}
+
+function usePinchGesture() {
+  const scaleRef = useRef(new Value<number>(1))
+  const handlePinch = useMemo(
+    () =>
+      event([
+        {
+          nativeEvent: ({ scale, state }: { scale: number; state: State }) =>
+            cond(eq(state, State.ACTIVE), set(scaleRef.current, scale), set(scaleRef.current, 1)),
+        },
+      ]),
+    [],
+  )
+
+  const transformStyle: ImageStyle = {
+    transform: [
+      {
+        scale: scaleRef.current as any,
+      },
+    ],
+  }
+
+  return {
+    handlePinch,
+    transformStyle,
+  }
+}
 
 export const ImageModal: React.FC<{ style?: ImageStyle; imageUri?: string; thumbnailUri?: string }> = ({
   style,
   imageUri,
   thumbnailUri,
 }) => {
-  // width / height
-  const [aspect, setAspect] = useState(1)
+  const imageSizeStyle = useImageSizeStyle(thumbnailUri)
 
-  useEffect(() => {
-    if (!thumbnailUri) return
-    Image.getSize(thumbnailUri, (w, h) => setAspect(w / h), () => {})
-  }, [thumbnailUri])
+  const { handlePinch, transformStyle } = usePinchGesture()
 
-  const { width: screenWidth, height: screenHeight } = Dimensions.get('screen')
-  const screenAspect = screenWidth / screenHeight
-
-  const imageStyle: ImageStyle =
-    aspect > screenAspect
-      ? {
-          width: screenWidth,
-          height: screenWidth / aspect,
-        }
-      : {
-          width: screenHeight * aspect,
-          height: screenHeight,
-        }
-
-  return <Image style={[imageStyle, style]} source={{ uri: imageUri }} />
+  return (
+    <PinchGestureHandler onGestureEvent={handlePinch} onHandlerStateChange={handlePinch}>
+      <Animated.View
+        style={[style, { width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center' }]}
+        collapsable={false}
+      >
+        <Animated.Image style={[imageSizeStyle, transformStyle]} source={{ uri: imageUri }} />
+      </Animated.View>
+    </PinchGestureHandler>
+  )
 }
--- src/components/templates/FileModalScreenView.tsx
+++ src/components/templates/FileModalScreenView.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { View, StatusBar } from 'react-native'
+import { View, StatusBar, ViewStyle } from 'react-native'
 import { Theme, withTheme, Appbar } from 'react-native-paper'
 import { ImageFile } from '../../models/file'
 import { ScreenView } from '../atoms/ScreenView'
 import { ImageModal } from '../molecules/ImageModal'
 import color from 'color'
+import { ScrollView } from 'react-native-gesture-handler'
 
 type Props = {
   file: ImageFile
@@ -12,36 +13,31 @@ type Props = {
 }
 
 const FileModalScreenViewImpl: React.FC<Props & { theme: Theme }> = ({ theme, file, onBackButtonPress }) => {
+  const backButtonStyle: ViewStyle = {
+    width: 32,
+    height: 32,
+    borderRadius: 16,
+    backgroundColor: color(theme.colors.background)
+      .alpha(0.5)
+      .string(),
+  }
+
   return (
     <ScreenView>
       <View style={{ position: 'absolute', top: StatusBar.currentHeight, zIndex: 100 }}>
-        <Appbar.BackAction
-          style={{
-            width: 32,
-            height: 32,
-            borderRadius: 16,
-            backgroundColor: color(theme.colors.background)
-              .alpha(0.5)
-              .string(),
-          }}
-          color={theme.colors.text}
-          size={24}
-          onPress={onBackButtonPress}
-        />
+        <Appbar.BackAction style={backButtonStyle} color={theme.colors.text} size={24} onPress={onBackButtonPress} />
       </View>
-      <View
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
+      <ScrollView
+        style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0 }}
+        contentContainerStyle={{ justifyContent: 'center', alignItems: 'center' }}
+        pagingEnabled
+        horizontal
+        pinchGestureEnabled
       >
-        <ImageModal imageUri={file.imageVariant.url} thumbnailUri={file.thumbnailVariant.url} />
-      </View>
+        {file.isImageFile() ? (
+          <ImageModal imageUri={file.imageVariant.url} thumbnailUri={file.thumbnailVariant.url} />
+        ) : null}
+      </ScrollView>
     </ScreenView>
   )
 }
