715d5103d88a08233547935f158cd7ffe209e5d0
Tosuke <tasukeprg@gmail.com>
2019-10-06 02:18:59 +0900

    feat(auth): use KeyboardAvoidingView in code input

--- src/components/templates/AuthorizeWithCodeScreenView.tsx
+++ src/components/templates/AuthorizeWithCodeScreenView.tsx
@@ -1,11 +1,13 @@
 import React, { useState, useCallback } from 'react'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, StatusBar } from 'react-native'
 import { Title, Caption, Divider, Button, Text, Snackbar } from 'react-native-paper'
 import { ScreenView } from '../atoms/ScreenView'
 import { Observable } from 'rxjs'
 import { useObservableEffect } from '../../hooks/useObservableEffect'
 import { useSnackBar } from '../../hooks/useSnackBar'
 import { PaperTextInput } from '../atoms/TextInput'
+import { KeyboardAvoidingView } from '../atoms/KeyboardAvoidingView'
+import { Header } from 'react-navigation'
 
 export const AuthorizeWithCodeScreenView: React.FC<{
   authorizing: boolean
@@ -31,33 +33,35 @@ export const AuthorizeWithCodeScreenView: React.FC<{
   )
 
   return (
-    <ScreenView style={styles.view}>
-      <Snackbar
-        visible={invalidCodeErrorBar.visible}
-        duration={invalidCodeErrorBar.duration}
-        onDismiss={invalidCodeErrorBar.dismiss}
-        action={invalidCodeErrorBar.action}
-      >
-        コードが不正です。
-      </Snackbar>
-      <Title>コードで認証</Title>
-      <Caption>認証画面でコードを貼り付けるように指示が出たらこの下に貼り付け、ボタンを押してください。</Caption>
-      <Divider style={styles.divider} />
-      <PaperTextInput
-        style={styles.textInput}
-        placeholder="code"
-        value={codeInput}
-        onChangeText={updateCodeInput}
-        onSubmitEditing={onAuthorizeButtonPressed}
-      />
-      <Button
-        mode="contained"
-        loading={authorizing}
-        disabled={authorizing || codeInput.length === 0}
-        onPress={onAuthorizeButtonPressed}
-      >
-        <Text>認証する</Text>
-      </Button>
+    <ScreenView>
+      <KeyboardAvoidingView style={styles.view} keyboardVerticalOffset={Header.HEIGHT + StatusBar.currentHeight!}>
+        <Snackbar
+          visible={invalidCodeErrorBar.visible}
+          duration={invalidCodeErrorBar.duration}
+          onDismiss={invalidCodeErrorBar.dismiss}
+          action={invalidCodeErrorBar.action}
+        >
+          コードが不正です。
+        </Snackbar>
+        <Title>コードで認証</Title>
+        <Caption>認証画面でコードを貼り付けるように指示が出たらこの下に貼り付け、ボタンを押してください。</Caption>
+        <Divider style={styles.divider} />
+        <PaperTextInput
+          style={styles.textInput}
+          placeholder="code"
+          value={codeInput}
+          onChangeText={updateCodeInput}
+          onSubmitEditing={onAuthorizeButtonPressed}
+        />
+        <Button
+          mode="contained"
+          loading={authorizing}
+          disabled={authorizing || codeInput.length === 0}
+          onPress={onAuthorizeButtonPressed}
+        >
+          <Text>認証する</Text>
+        </Button>
+      </KeyboardAvoidingView>
     </ScreenView>
   )
 }
