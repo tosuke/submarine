a2489f0dac3057b9fa7bf50a927593843ac9e030
Tosuke <tasukeprg@gmail.com>
2020-01-02 12:26:29 +0900

    feat: implement collapsible header

--- src/view/pages/HomeScreen/MainView.tsx
+++ src/view/pages/HomeScreen/MainView.tsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useCallback, useRef, forwardRef, useMemo, useImperativeHandle } from 'react'
 import styled from 'styled-components/native'
-import { Platform, StyleSheet, FlatList } from 'react-native'
+import {
+  Animated,
+  Platform,
+  StyleSheet,
+  FlatList,
+  View,
+  FlatListProps,
+  RefreshControl,
+  RefreshControlProps,
+  NativeSyntheticEvent,
+  NativeScrollEvent,
+} from 'react-native'
 import { Appbar } from 'react-native-paper'
+import { Header as NavigationHeader } from 'react-navigation'
 import { MaterialIcons } from '@expo/vector-icons'
 import { TimelineBloc } from '../../../blocs/publicTimelineBloc'
 import { useObservable } from '../../../hooks/useObservable'
 import { ScreenView, AppHeader, PrimaryFAB } from '../../design'
 import { TimelineBlocContext } from '../../../hooks/inject'
 import { useTimeline } from '../../modules/useTimeline'
-import { File } from '../../../models'
+import { File, Post } from '../../../models'
+import { getStatusBarHeight } from 'react-native-status-bar-height'
+import { useTheme } from '../../../hooks/useTheme'
+
+const HEADER_HEIGHT = NavigationHeader.HEIGHT
 
 export const Header: React.FC<{ onTouchEnd?: () => void; connectedToStream?: boolean }> = ({
   onTouchEnd,
@@ -30,9 +46,20 @@ const SendIcon: React.FC<{ size?: number; color?: string }> = ({ size, color }) 
 
 export const FAB: React.FC<{ onPress?: () => void }> = ({ onPress }) => <PrimaryFAB icon={SendIcon} onPress={onPress} />
 
-const TimelineWrapper = styled.View`
-  flex: 1;
-`
+const AnimatedFlatList = forwardRef((props: FlatListProps<Post>, ref: React.Ref<FlatList<Post>>) => {
+  const nodeRef = useRef<{ node: FlatList<Post> | null }>({ node: null })
+  useImperativeHandle(ref, () => nodeRef.current.node!)
+
+  const animatedRefCallback = useCallback(
+    (node: { getNode(): FlatList<Post> | null } | null) => {
+      if (node == null) return
+      nodeRef.current.node = node.getNode()
+    },
+    [nodeRef],
+  )
+
+  return <Animated.FlatList ref={animatedRefCallback} {...props} />
+})
 
 export const MainView: React.FC<{
   timelineBloc: TimelineBloc
@@ -40,17 +67,78 @@ export const MainView: React.FC<{
   navigateToFileModal: (files: File[], index: number) => void
   openUrl: (url: string) => void
 }> = ({ timelineBloc, onPostButtonPress, navigateToFileModal, openUrl }) => {
-  const { flatListRef, flatListProps, scrollToTop } = useTimeline({
+  const statusBarHeight = getStatusBarHeight()
+
+  const TimelineRefreshControl = useCallback(
+    (props: RefreshControlProps) => <RefreshControl {...props} progressViewOffset={HEADER_HEIGHT + statusBarHeight} />,
+    [statusBarHeight],
+  )
+
+  const { flatListRef, flatListProps } = useTimeline({
     timelineBloc,
     navigateToFileModal,
     openUrl,
+    RefreshControl: TimelineRefreshControl,
   })
+
+  const scrollToTop = useCallback(() => {
+    if (flatListRef.current == null) return
+    flatListRef.current.scrollToIndex({
+      index: 0,
+      viewOffset: HEADER_HEIGHT + statusBarHeight,
+      animated: true,
+    })
+  }, [flatListRef])
 
   const connected = useObservable(() => timelineBloc.connectedToSocket$, false, [timelineBloc])
 
+  const { current: scrollY } = useRef(new Animated.Value(0))
+  const { current: headerY } = useRef(Animated.diffClamp(Animated.multiply(-1, scrollY), -HEADER_HEIGHT, 0))
+
+  const onScroll = useMemo(
+    () =>
+      Animated.event<NativeSyntheticEvent<NativeScrollEvent>>(
+        [
+          {
+            nativeEvent: {
+              contentOffset: {
+                y: scrollY,
+              },
+            },
+          },
+        ],
+        { useNativeDriver: true },
+      ),
+    [scrollY],
+  )
+
+  const theme = useTheme()
+
   const HeaderAndFAB = (
     <>
-      <Header onTouchEnd={scrollToTop} connectedToStream={connected} />
+      <View
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          height: statusBarHeight,
+          backgroundColor: theme.colors.background,
+          zIndex: 3,
+        }}
+      />
+      <Animated.View
+        style={{
+          transform: [
+            {
+              translateY: headerY,
+            },
+          ],
+          zIndex: 2,
+        }}
+      >
+        <Header onTouchEnd={scrollToTop} connectedToStream={connected} />
+      </Animated.View>
       <FAB onPress={onPostButtonPress} />
     </>
   )
@@ -60,9 +148,18 @@ export const MainView: React.FC<{
       {Platform.OS !== 'android' && HeaderAndFAB}
       <ScreenView style={StyleSheet.absoluteFill}>
         {Platform.OS === 'android' && HeaderAndFAB}
-        <TimelineWrapper>
-          <FlatList ref={flatListRef} {...flatListProps} />
-        </TimelineWrapper>
+        <AnimatedFlatList
+          ref={flatListRef}
+          {...flatListProps}
+          bounces={false}
+          overScrollMode="never"
+          style={StyleSheet.absoluteFill}
+          contentContainerStyle={{
+            paddingTop: HEADER_HEIGHT + getStatusBarHeight(),
+          }}
+          ListFooterComponentStyle={{ paddingBottom: HEADER_HEIGHT }}
+          onScroll={onScroll}
+        />
       </ScreenView>
     </TimelineBlocContext.Provider>
   )
