a7e7a825e42a78cdf030ae7eb0920e1e1eac4365
Tosuke <tasukeprg@gmail.com>
2020-01-04 15:43:56 +0900

    refactor(view): use react-native-paper's useTheme hook

--- src/hooks/useTheme.ts
+++
@@ -1,9 +0,0 @@
-import { Theme } from 'react-native-paper'
-import { useThemeBloc } from './inject/useThemeBloc'
-import { useValueObservable } from './useObservable'
-
-export function useTheme(): Theme {
-  const themeBloc = useThemeBloc()
-  const theme = useValueObservable(() => themeBloc.theme$)
-  return theme
-}
