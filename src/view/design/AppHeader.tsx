22fb56ec55c21755b9d66e68ed8b82075a125e5a
Tosuke <tasukeprg@gmail.com>
2020-01-07 01:14:16 +0900

    feat: fix AppHeader height

--- src/view/design/AppHeader.tsx
+++ src/view/design/AppHeader.tsx
@@ -2,10 +2,11 @@ import styled from 'styled-components/native'
 import { Appbar } from 'react-native-paper'
 import { StyleSheet } from 'react-native'
 import { dividerColor } from './color'
+import { APPBAR_HEIGHT } from '../constants/header'
 
 export const AppHeader = styled(Appbar.Header)`
   background-color: ${props => props.theme.colors.background};
-  elevation: 4;
   border-bottom-color: ${props => dividerColor(props.theme)};
   border-bottom-width: ${StyleSheet.hairlineWidth * 2};
+  height: ${APPBAR_HEIGHT};
 `
