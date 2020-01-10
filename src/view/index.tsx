66d5d77d3d40566678f4f7941df62f113b33ea13
Nitta Keisuke <tasukeprg@gmail.com>
2020-01-11 02:54:11 +0900

    force call

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react'
+   import React, { useMemo } from 'react'
 import { View } from 'react-native'
 import { NavigationNativeContainer } from '@react-navigation/native'
 import { useTheme } from 'react-native-paper'
