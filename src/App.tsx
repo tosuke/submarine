0090473c8816f96bebab2112834baf2d90444d71
Tosuke <tasukeprg@gmail.com>
2020-01-15 15:55:09 +0900

    fix: remove unneeded imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { enableScreens } from 'react-native-screens'
 import { AppView } from './view'
-import { StatusBar } from './view/design/StatusBar'
 import { DarkTheme } from './view/constants/theme'
 import { AuthBlocProvider, ThemeBlocProvider, PostSendBlocProvider } from './hooks/inject'
 
