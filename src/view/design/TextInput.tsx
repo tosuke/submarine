11dd6287fe0146544db0b191d6e75e86378ad290
Tosuke <tasukeprg@gmail.com>
2020-01-18 03:11:17 +0900

    feat: extend TextInput

--- src/view/design/TextInput.tsx
+++ src/view/design/TextInput.tsx
@@ -4,43 +4,51 @@ import { TextInput as NativePaperTextInput, useTheme } from 'react-native-paper'
 
 export type TextInputProps = NativeTextInputProps
 
-export const TextInput = forwardRef(({ ...rest }: TextInputProps, ref: React.Ref<NativeTextInput>) => {
-  const theme = useTheme()
-  const textInputStyle: TextStyle = { color: theme.colors.text }
-  const placeholderTextColor = rest.placeholderTextColor || theme.colors.text
-  const keyboardAppearance = rest.keyboardAppearance || (theme.dark ? 'dark' : 'default')
+export const TextInput = forwardRef(
+  (
+    {
+      Component = NativeTextInput,
+      ...rest
+    }: TextInputProps & { Component?: React.ComponentType<TextInputProps & React.RefAttributes<NativeTextInput>> },
+    ref: React.Ref<NativeTextInput>,
+  ) => {
+    const theme = useTheme()
+    const textInputStyle: TextStyle = { color: theme.colors.text }
+    const placeholderTextColor = rest.placeholderTextColor || theme.colors.text
+    const keyboardAppearance = rest.keyboardAppearance || (theme.dark ? 'dark' : 'default')
 
-  // https://github.com/facebook/react-native/issues/20887
-  const textInputRef = useRef<NativeTextInput>(null)
-  useImperativeHandle(ref, () => textInputRef.current!, [textInputRef])
+    // https://github.com/facebook/react-native/issues/20887
+    const textInputRef = useRef<NativeTextInput>(null)
+    useImperativeHandle(ref, () => textInputRef.current!, [textInputRef])
 
-  const [editable, setEditable] = useState(rest.editable !== false)
+    const [editable, setEditable] = useState(rest.editable !== false)
 
-  useEffect(() => {
-    const initial = rest.editable !== false
-    setEditable(!initial)
-    const id = setTimeout(() => {
-      setEditable(initial)
-      if (textInputRef.current && rest.autoFocus) textInputRef.current.focus()
-    }, 100)
-    return () => {
-      setEditable(initial)
-      clearTimeout(id)
-    }
-  }, [rest.editable])
+    useEffect(() => {
+      const initial = rest.editable !== false
+      setEditable(!initial)
+      const id = setTimeout(() => {
+        setEditable(initial)
+        if (textInputRef.current && rest.autoFocus) textInputRef.current.focus()
+      }, 100)
+      return () => {
+        setEditable(initial)
+        clearTimeout(id)
+      }
+    }, [rest.editable])
 
-  return (
-    <NativeTextInput
-      {...rest}
-      autoFocus={false}
-      ref={textInputRef}
-      editable={editable}
-      style={[textInputStyle, rest.style]}
-      keyboardAppearance={keyboardAppearance}
-      placeholderTextColor={placeholderTextColor}
-    />
-  )
-})
+    return (
+      <Component
+        {...rest}
+        autoFocus={false}
+        ref={textInputRef}
+        editable={editable}
+        style={[textInputStyle, rest.style]}
+        keyboardAppearance={keyboardAppearance}
+        placeholderTextColor={placeholderTextColor}
+      />
+    )
+  },
+)
 
 export type PaperTextInputProps = React.PropTypeOf<typeof NativePaperTextInput>
 export const PaperTextInput = (props: PaperTextInputProps) => {
