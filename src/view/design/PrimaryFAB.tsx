6276a414af9cbfdb35e4feba63455f7528c933f0
Tosuke <tasukeprg@gmail.com>
2020-01-21 19:08:46 +0900

    refactor

--- src/view/design/PrimaryFAB.tsx
+++ src/view/design/PrimaryFAB.tsx
@@ -5,9 +5,8 @@ import { FAB, Theme, withTheme } from 'react-native-paper'
 const PrimaryFABImpl: React.FC<React.PropTypeOf<typeof FAB> & { theme: Theme }> = ({ theme, style, ...props }) => {
   const fabStyle: ViewStyle = {
     position: 'absolute',
-    right: 0,
-    bottom: 0,
-    margin: 16,
+    right: 16,
+    bottom: 16,
     zIndex: 100,
     backgroundColor: theme.colors.primary,
   }
