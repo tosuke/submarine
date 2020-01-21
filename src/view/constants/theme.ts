b4e8d10a4bc44c9e3e0b333c0967eda124f0e551
Tosuke <tasukeprg@gmail.com>
2020-01-21 21:03:14 +0900

    feat: add light theme

--- src/view/constants/theme.ts
+++ src/view/constants/theme.ts
@@ -1,5 +1,18 @@
 import { Theme, DarkTheme as PaperDarkTheme, DefaultTheme } from 'react-native-paper'
 
+export const LightTheme: Theme = {
+  ...DefaultTheme,
+  dark: false,
+  mode: 'adaptive',
+  roundness: 4,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#039be5',
+    accent: '#388e3c',
+    error: '#ff1744',
+  },
+}
+
 export const DarkTheme: Theme = {
   ...DefaultTheme,
   dark: true,
