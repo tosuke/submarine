8c9bd67331d40bfccba24f9164214d8a255fe0a9
Tosuke <tasukeprg@gmail.com>
2020-01-26 19:30:53 +0900

    fix: fix types

--- src/view/design/CheckBox/NativeCheckBoxAndroid.tsx
+++ src/view/design/CheckBox/NativeCheckBoxAndroid.tsx
@@ -1,2 +1,4 @@
-export { default as NativeCheckBox } from '@react-native-community/checkbox'
+import React from 'react'
+import { CheckBoxProps } from 'react-native'
 export { CheckBoxProps as NativeCheckBoxProps } from 'react-native'
+export const NativeCheckBox: React.ComponentClass<CheckBoxProps> = require('@react-native-community/checkbox')
