6bde4c487d1f056b215b01d82561f85cf783429e
Tosuke <tasukeprg@gmail.com>
2020-01-29 20:33:36 +0900

    fix: disable LayoutAnimation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { Suspense } from 'react'
-import { Platform, UIManager } from 'react-native'
 import { enableScreens } from 'react-native-screens'
 import { AppearanceProvider } from 'react-native-appearance'
 import { AppView } from './view'
@@ -8,10 +7,6 @@ import { DarkTheme, LightTheme } from './view/constants/theme'
 import { AuthBlocProvider, ThemeProvider, PostSendBlocProvider, PreferenceProvider } from './hooks/inject'
 
 enableScreens()
-
-if (Platform.OS === 'android' && UIManager.setLayoutAnimationEnabledExperimental) {
-  UIManager.setLayoutAnimationEnabledExperimental(true)
-}
 
 export default class App extends React.Component {
   render() {
